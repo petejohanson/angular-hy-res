26b4eb1317e6eded40e5f73c1aa6e35d9ac9a68b
Peter Johanson <peter@peterjohanson.com>
2014-08-27 21:37:32 -0400

    Fix dist to be latest code.

--- dist/angular-hyper-resource-full.js
+++ dist/angular-hyper-resource-full.js
@@ -1,6 +1,6 @@
 /**
  * angular-hyper-resource
- * @version v0.0.1 - 2014-08-26
+ * @version v0.0.1 - 2014-08-27
  * @link https://github.com/petejohanson/angular-hyper-resource
  * @author Pete Johanson <latexer@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -58,12 +58,21 @@ angular.module('angular-hyper-resource', [])
           return this.$$embedded[rel];
         };
 
-        this.$follow = function(rel) {
-          var link = this.$link(rel);
+        this.$followLink = function(link, options) {
           if(link === null) {
             return null; // TODO: Something else to return? Resource w/ rejected promise and error?
           }
-          return Resource.get(link);
+
+          return Resource.get(link, options);
+        };
+
+        this.$follow = function(rel, options) {
+          // This resource may not be resolved yet,
+          // so we follow a *future* link by chaining our
+          // own promise.
+          return this.$followLink(this.$promise.then(function(r) {
+            return r.$link(rel);
+          }), options);
         };
       };
 
@@ -78,11 +87,19 @@ angular.module('angular-hyper-resource', [])
 
       Resource.get = function(link, options) {
         var res = new Resource();
-        var url = link.href;
-        var httpConfig = angular.extend(options || {}, { url: url });
 
         res.$promise =
-          $http(httpConfig)
+          $q.when(link)
+          .then(function(l) {
+            var url = l.href;
+    
+            if (l.templated) {
+              url = new URITemplate(url).expand(options.data);
+            }
+
+            var httpConfig = angular.extend(options || {}, { url: url });
+            return $http(httpConfig);
+          })
           .then(function(response) {
             res.$$resolve(response.data, response.headers);
             return res;
@@ -92,77 +109,6 @@ angular.module('angular-hyper-resource', [])
 
         return res;
       };
-
-/*
-      var Resource = function(data, links, embedded, parser) {
-        this.embedded = {};
-        angular.extend(this, data);
-        
-        this.$link = function(rel) {
-          if (!links.hasOwnProperty(rel)) {
-            return null;
-          }
-          
-          return links[rel];
-        };
-
-        this.$embedded = function(rel) {
-          if(!this.embedded.hasOwnProperty(rel)) {
-            return null;
-          }
-
-          return this.embedded[rel];
-        };
-
-        this.$follow = function(rel, options) {
-          var link = this.$link(rel);
-          if (link === null) {
-            return $q.reject('Link relation not found');
-          }
-          var url = link.href;
-          if (link.templated) {
-            url = new URITemplate(url).expand(options.data);
-          }
-
-          return $http(angular.extend(options || {}, { url: url }))
-            .then(function(resp) {
-              if (resp.headers('Content-Type') === 'application/hal+json') {
-                resp.data = parser.parse(resp.data);
-              }
-              return resp;
-            });
-        };
-
-        for (var name in embedded) {
-          var e = embedded[name];
-          if (angular.isArray(e)) {
-            var arr = [];
-            for(var i = 0, len = e.length; i< len; ++i) {
-              arr.push(parser.parse(e[i]));
-            }
-            this.embedded[name] = arr;
-          } else {
-            this.embedded[name] = parser.parse(e);
-          }
-        }
-      };
-
-      var Parser = function() {
-        this.parse = function(s) {
-          if (angular.isString(s)) {
-            s = JSON.parse(s);
-          }
-  
-          var links = s._links;
-          var embedded = s._embedded;
-  
-          delete s._links;
-          delete s._embedded;
-  
-          return new Resource(s, links, embedded, this);
-        };
-      };
-*/
 
       var hrResourceFactory = function(url, options) {
         return {
