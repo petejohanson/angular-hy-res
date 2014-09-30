1cfe1545f9d68d344479f3b5a16a74e56441f0ff
Peter Johanson <peter@peterjohanson.com>
2014-09-30 13:31:21 -0400

    Initial work on Siren integration.
    * Initial link parsing support.
    * Basic data support.
    * TODO:
        * Sub-Entities.
        * Actions.

--- dist/angular-hy-res-full.js
+++ dist/angular-hy-res-full.js
@@ -111,6 +111,59 @@ angular.module('angular-hy-res-link-header', ['angular-hy-res'])
 
 'use strict';
 
+angular.module('angular-hy-res-siren', ['angular-hy-res'])
+  .provider('hrSirenExtension', function() {
+    this.mediaTypes = ['application/vnd.siren+json'];
+    this.$get = function(hrWebLinkFactory, hrLinkCollection) {
+      var mediaTypeSet = {};
+      for (var i = 0; i < this.mediaTypes.length; i++) {
+        mediaTypeSet[this.mediaTypes[i]] = true;
+      }
+
+      var SirenExtension = function() {
+        this.applies = function(data, headers) {
+          return mediaTypeSet[headers('Content-Type')] !==  undefined;
+        };
+
+        this.dataParser = function(data, headers) {
+          var ret = data.properties || {};
+          if (data.title) {
+            ret.title = data.title;
+          }
+
+          return ret;
+        };
+
+        this.linkParser = function(data, headers, Resource) {
+          if (!angular.isObject(data.links)) {
+            return {};
+          }
+
+          var ret = {};
+          angular.forEach(data.links, function(val) {
+            var link = hrWebLinkFactory(val);
+            for (var li = 0; li < val.rel.length; li++) {
+              ret[val.rel[li]] = link;
+            }
+          });
+          return ret;
+        };
+
+        this.embeddedParser = function(data, headers) {
+          return {};
+        };
+      };
+
+      return new SirenExtension();
+    };
+  })
+  .config(['hrResourceProvider', function(hrResourceProvider) {
+    hrResourceProvider.extensions.push('hrSirenExtension');
+  }]);
+
+
+'use strict';
+
 var WebLink = function(data, $http, Resource, URITemplate) {
   angular.extend(this, data);
   this.$$http = $http;
---
+++ test/spec/siren-tests.js
@@ -0,0 +1,87 @@
+'use strict';
+
+describe('angular-hy-res: hrSirenExtension', function () {
+  describe('the extension', function() {
+    var hrSirenExtension;
+
+    // load the controller's module
+    beforeEach(angular.mock.module('angular-hy-res-siren'));
+
+    beforeEach(angular.mock.inject(function(_hrSirenExtension_) {
+      hrSirenExtension = _hrSirenExtension_;
+    }));
+
+    describe('extension applicability', function() {
+      it('should apply to application/vnd.siren+json content type', function() {
+        expect(hrSirenExtension.applies({}, function(header) {
+          return header === 'Content-Type' ? 'application/vnd.siren+json' : null;
+        }, 200)).toBe(true);
+      });
+    });
+    describe('links parser', function() {
+      it('should return the basic link', function() {
+        var links = hrSirenExtension.linkParser({links: [ { rel: ['self'], href: '/orders/123' } ] }, {}, 200);
+        expect(links.self.href).toEqual('/orders/123');
+      });
+
+      it('should return link for each relation in rel array', function() {
+        var links = hrSirenExtension.linkParser({links: [ { rel: ['self', 'order'], href: '/orders/123' } ] }, {}, 200);
+        expect(links.self.href).toEqual('/orders/123');
+        expect(links.order.href).toEqual('/orders/123');
+      });
+
+      xit('should return a link array for duplicate link rels', function() {
+        var links = hrSirenExtension.linkParser({
+          links: [
+            { rel: ['self'], href: '/orders/123' },
+            { rel: ['section'], href: '/orders?page=2' },
+            { rel: ['section'], href: '/orders?page=3' }
+          ]
+        },{}, 200);
+        expect(links.section.length).toBe(2);
+      });
+    });
+
+    describe('data parser', function() {
+      it('should return the properties field values', function() {
+        var data = hrSirenExtension.dataParser({
+          links: [ { rel: ['self'], href: '/orders/123' } ],
+          properties: {
+            name: 'John Doe'
+          }
+        }, {});
+
+        expect(data).toEqual({ name: 'John Doe' });
+      });
+
+      it('should include the title, if present', function() {
+        var data = hrSirenExtension.dataParser({
+          links: [ { rel: ['self'], href: '/orders/123' } ],
+          properties: {
+            name: 'John Doe'
+          },
+          title: 'My Order #123'
+        }, {});
+
+        expect(data).toEqual({
+          name: 'John Doe',
+          title: 'My Order #123'
+        });
+      });
+    });
+  });
+  //
+  //describe('the provider', function() {
+  //  it('handles additional media types', function() {
+  //    angular.mock.module('angular-hy-res-hal', function(hrHalExtensionProvider) {
+  //      hrHalExtensionProvider.mediaTypes.push('application/vnd.myco.blog');
+  //    });
+  //
+  //    inject(function(hrHalExtension) {
+  //      expect(hrHalExtension.applies({}, function(header) {
+  //        return header === 'Content-Type' ? 'application/vnd.myco.blog' : null;
+  //      })).toBeTruthy();
+  //    });
+  //  });
+  //});
+});
