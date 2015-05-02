74978365e3e89eca64906e3f1fee667f9ef97b43
Peter Johanson <peter@peterjohanson.com>
2015-05-01 22:20:53 -0400

    Karma fixes.

--- karma.conf.js
+++ karma.conf.js
@@ -62,10 +62,11 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
+    browserNoActivityTimeout: 30000,
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ['PhantomJS', 'Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
