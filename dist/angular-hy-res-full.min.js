b35600f6d8cad866c5ad63d6b492857854b1d770
Peter Johason <peter@peterjohanson.com>
2015-06-06 01:01:23 -0400

    Release v0.0.19

--- dist/angular-hy-res-full.min.js
+++ dist/angular-hy-res-full.min.js
@@ -1,6 +1,6 @@
 /**
  * angular-hy-res - Hypermedia client for AngularJS inspired by $resource
- * @version v0.0.18 - 2015-06-05
+ * @version v0.0.19 - 2015-06-06
  * @link https://github.com/petejohanson/angular-hy-res
  * @author Pete Johanson <peter@peterjohanson.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
