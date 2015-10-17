83ae3fb3167b028dc94802886416c60caa6104b7
Peter Johason <peter@peterjohanson.com>
2015-10-17 00:05:12 -0400

    Release v0.0.28

--- dist/hal.min.js
+++ dist/hal.min.js
@@ -1,6 +1,6 @@
 /**
  * angular-hy-res - Hypermedia client for AngularJS inspired by $resource
- * @version v0.0.27 - 2015-10-16
+ * @version v0.0.28 - 2015-10-17
  * @link https://github.com/petejohanson/angular-hy-res
  * @author Pete Johanson <peter@peterjohanson.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
