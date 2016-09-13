14444d612662cfdc9d46fde67cd4b7b5c13bf103
Peter Johason <peter@peterjohanson.com>
2016-09-13 13:21:40 -0400

    Bump to hy-res@0.0.26.

--- dist/core.min.js
+++ dist/core.min.js
@@ -1,6 +1,6 @@
 /**
  * angular-hy-res - Hypermedia client for AngularJS inspired by $resource
- * @version v0.0.31 - 2016-06-10
+ * @version v0.0.31 - 2016-09-13
  * @link https://github.com/petejohanson/angular-hy-res
  * @author Pete Johanson <peter@peterjohanson.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
